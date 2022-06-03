d45d8460526f5a44e6d9d4303a122d0e3380be59
far-art <arturfarmanov91@gmail.com>
2022-06-03 10:10:45 +0300

    added two components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminPanelComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule
