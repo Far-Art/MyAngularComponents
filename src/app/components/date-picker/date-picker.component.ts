afa6868d6311a2d60d7fca90f1a3cdf00d4e0687
far-art <arturfarmanov91@gmail.com>
2024-10-19 19:17:28 +0300

    work on datepicker

--- src/app/components/date-picker/date-picker.component.ts
+++ src/app/components/date-picker/date-picker.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 
 
 @Component({
@@ -9,6 +9,7 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 export class DatePickerComponent implements OnInit {
 
   formattedValue: string = '';
+
   readonly dateConfig = {
     day: {
       min: 1,
@@ -35,7 +36,7 @@ export class DatePickerComponent implements OnInit {
 
   @ViewChild('dateInput', {static: true}) dateInput!: ElementRef<HTMLInputElement>;
 
-  value: string | null = null;
+  value: string = '';
   private readonly dateFormat: string = '__ / __ / ____';
   private selectionStart = 0;
   private selectionEnd = 0;
@@ -46,26 +47,15 @@ export class DatePickerComponent implements OnInit {
     this.formattedValue = this.dateFormat;
   }
 
-  setValue(event: Event): void {
-
-  }
-
-  onInput(event: InputEvent): void {
-    if (event.inputType?.includes('delete')) {
-      this.handleDelete(event);
-    } else {
-      this.handleInsert(event);
-    }
-
-  }
-
   setCaret(): void {
     const input = this.dateInput.nativeElement;
-    // const _index = this.formattedDate.indexOf('_');
+    const _index = this.formattedValue.indexOf('_');
 
-    // if (_index !== -1) {
-    //   input.setSelectionRange(_index, _index); // Move caret
-    // }
+    if (_index !== -1) {
+      input.setSelectionRange(_index, _index); // Move caret
+    }
+    // console.log('start:', this.selectionStart)
+    // console.log('  end:', this.selectionEnd)
   }
 
   onFocus(event: FocusEvent): void {
@@ -76,43 +66,86 @@ export class DatePickerComponent implements OnInit {
   onSelectionChange(target: HTMLInputElement): void {
     this.selectionStart = target.selectionStart ?? 0;
     this.selectionEnd = target.selectionEnd ?? 0;
-    // console.log('start:', this.selectionStart)
-    // console.log('  end:', this.selectionEnd)
+
+    console.log(this.formattedValue[this.selectionStart - 1])
+
   }
 
-  private handleInsert(event: InputEvent): void {
-    this.updateFormatedValue(event);
-    // this.setCaret();
+  @HostListener('keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent): void {
+    const allowedKeys = ['Backspace', 'Enter', 'Delete', 'Home', 'End', 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'];
+    if (event.key.match(/\D/) && !allowedKeys.includes(event.key)) {
+      event.preventDefault();
+      return;
+    }
+
+    if (event.key === 'ArrowLeft' || event.key === 'ArrowRight' || event.key === 'Home' || event.key === 'End') {
+      // TODO handle caret position on blank spaces
+      if (event.key === 'End' || (event.key === 'ArrowRight' || event.key === 'ArrowLeft') && this.formattedValue.substring(this.selectionStart, this.selectionEnd + 1)?.match(/\D/)) {
+        event.preventDefault();
+        if (event.key === 'ArrowLeft') {
+          this.dateInput.nativeElement.setSelectionRange(this.selectionStart - 3, this.selectionEnd - 3);
+        } else {
+          this.setCaret();
+        }
+      }
+      return;
+    }
+
+    if (allowedKeys.includes(event.key)) {
+      event.preventDefault();
+    }
+
+    if (event.key === 'Backspace' || event.key === 'Delete') {
+      this.handleDelete(event.key);
+      return;
+    }
+
+    if (event.key.match(/\d/)) {
+      event.preventDefault();
+      this.handleInsert(event.key);
+    }
+
   }
 
-  private handleDelete(event: InputEvent): void {
+  private handleInsert(key: string): void {
+    // TODO only append to value if it is valid
+    this.value += key;
+    this.updateFormatedValue();
+    this.setCaret();
   }
 
-  private updateFormatedValue(event: InputEvent): void {
-    const el = event.target as HTMLInputElement;
-    console.log(el.placeholder);
-    this.setDay(event)
-    this.setMonth(event);
-    this.setYear(event);
+  private handleDelete(key: string): void {
+    console.log('_'.repeat(1))
+    console.log('delete')
+    console.log(this.selectionStart)
+    console.log(this.selectionEnd)
+    this.setCaret();
   }
 
-  private setDay(event: Event): void {
-    const value = this.getValue(event).substring(0, 2);
-    if (+value >= this.dateConfig.day.min && +value <= this.dateConfig.day.max) {
+  private updateFormatedValue(): void {
+    this.setDay()
+    this.setMonth();
+    this.setYear();
+  }
+
+  private setDay(): void {
+    const value = this.value.substring(0, 2);
+    if (this.validateDay(value)) {
       this.formattedValue = this.replaceRange(this.dateConfig.day.start, this.dateConfig.day.end, value);
     }
   }
 
-  private setMonth(event: Event): void {
-    const value = this.getValue(event).substring(2, 4);
-    if (+value >= this.dateConfig.month.min && +value <= this.dateConfig.month.max) {
+  private setMonth(): void {
+    const value = this.value.substring(2, 4);
+    if (this.validateMonth(value)) {
       this.formattedValue = this.replaceRange(this.dateConfig.month.start, this.dateConfig.month.end, value);
     }
   }
 
-  private setYear(event: Event): void {
-    const value = this.getValue(event).substring(4, 12);
-    if (+value >= this.dateConfig.year.min && +value <= this.dateConfig.year.max) {
+  private setYear(): void {
+    const value = this.value.substring(4, 12);
+    if (this.validateYear(value)) {
       this.formattedValue = this.replaceRange(this.dateConfig.year.start, this.dateConfig.year.end, value);
     }
   }
@@ -122,9 +155,19 @@ export class DatePickerComponent implements OnInit {
     return this.formattedValue.substring(0, start) + substitute + this.formattedValue.substring(start + endLen);
   }
 
-  private getValue(event: Event): string {
-    const rawValue = (event.target as HTMLInputElement).value;
-    return rawValue.replace(/\D/g, '');
+  private validateDay(value: string): boolean {
+    if (!value) return false;
+    return +value[0] <= +`${this.dateConfig.day.max}`[0] && +value >= this.dateConfig.day.min && +value <= this.dateConfig.day.max;
+  }
+
+  private validateMonth(value: string): boolean {
+    if (!value) return false;
+    return +value[0] <= +`${this.dateConfig.month.max}`[0] && +value >= this.dateConfig.month.min && +value <= this.dateConfig.month.max;
+  }
+
+  private validateYear(value: string): boolean {
+    if (!value) return false;
+    return +value[0] <= +`${this.dateConfig.year.max}`[0] && +value >= this.dateConfig.year.min && +value <= this.dateConfig.year.max;
   }
 
 }
