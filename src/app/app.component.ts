5bc29df80a8e06390e7df0a58520aa641208e4e3
far-art <arturfarmanov91@gmail.com>
2024-10-24 12:03:40 +0300

    1.radio button styled 2.added service to snapshot html elements to separate file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import {ElementCaptureService} from './services/element-capture.service';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'My-Angular-Components';
+
+  constructor(private elementCapture: ElementCaptureService) { }
+
+  ngAfterViewInit(): void {
+
+  }
+
 }
